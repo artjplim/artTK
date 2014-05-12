c7a5ecc6861016a5b8952873a5785c7c7acc9342
Art Lim <artjplim@gmail.com>
2014-05-12 10:15:32 +0800

    newest

--- src/main/java/my/groupid/myteam/MyTeamController.java
+++ src/main/java/my/groupid/myteam/MyTeamController.java
@@ -1,13 +1,16 @@
 package my.groupid.about;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyTeamController {
 
 	@RequestMapping(value = "myteam")
-	public String myteam() {
+	public String myteam(Model model) {
+		String Venus = "haha";
+		model.addAttribute("name", Venus);
         return "myteam/myteam";
     }
 }
